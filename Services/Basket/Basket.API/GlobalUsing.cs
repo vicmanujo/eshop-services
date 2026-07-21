d64c2a8942828fa09eb2bad71348b9bc5e151140
vicmanujo <josevictormanuel619@gmail.com>
2026-07-21 13:18:21 -0600

    Estructura limpia: Solo BuildingBlocks y Catalog API

--- Services/Basket/Basket.API/GlobalUsing.cs
+++
@@ -1,8 +0,0 @@
-global using Basket.API.Models;
-global using BuildingBlocks.CQRS;
-global using BuildingBlocks.Exceptions;
-global using BuildingBlocks.Behaviors;
-global using Basket.API.Data;
-global using Carter;
-global using MediatR;
-global using Mapster;
